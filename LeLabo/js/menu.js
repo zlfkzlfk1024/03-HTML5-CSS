3aba8c5b300168f57005688fedbdb4f6461442f1
yuj <zlfkzlfk1024@naver.com>
2024-03-12 15:10:28 +0900

    first commit

---
+++ LeLabo/js/menu.js
@@ -0,0 +1,18 @@
+let lnb = document.querySelector(".lnb");
+let gnb = document.querySelectorAll(".menu > li");
+let sub = document.querySelectorAll(".sub");
+function menu_in() {
+  lnb.classList.add("on");
+  sub.classList.add("on");
+}
+function menu_out() {
+  lnb.classList.remove("on");
+}
+gnb.forEach(function (value, index) {
+  value.onmouseenter = function () {
+    menu_in();
+  };
+  value.onmouseleave = function () {
+    menu_out();
+  };
+});
