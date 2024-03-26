7a194ab6bd84075ae8a9d076d8e9152b8759c158
yuj <zlfkzlfk1024@naver.com>
2024-03-26 16:41:59 +0900

    first commit

---
+++ samdasoo/js/index.js
@@ -0,0 +1,45 @@
+let ani1 = gsap.timeline();
+ani1.to(".t2", {
+  autoAlpha: 1,
+});
+ScrollTrigger.create({
+  animation: ani1,
+  trigger: ".img1 img:last-child",
+  start: "top top",
+  end: "+=800",
+  scrub: true,
+});
+let ani2 = gsap.timeline();
+ani2.to(".t3", {
+  autoAlpha: 1,
+});
+ScrollTrigger.create({
+  animation: ani2,
+  trigger: ".img2 img:last-child",
+  start: "top top",
+  end: "+=800",
+  scrub: true,
+});
+let ani3 = gsap.timeline();
+ani2.to(".sam", {
+  autoAlpha: 0,
+});
+ScrollTrigger.create({
+  animation: ani2,
+  trigger: ".wrap",
+  start: "top top",
+  end: ".section3",
+  scrub: true,
+});
+let ani4 = gsap.timeline();
+ani4.to(".lang p", {
+  color: "#fff",
+});
+ScrollTrigger.create({
+  animation: ani4,
+  trigger: ".section2",
+  start: "top top",
+  end: ".section3",
+  scrub: true,
+  markers: true,
+});
